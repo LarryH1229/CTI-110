d2c0cc456d9c5b5f5b6281c04fd8234249d63b5e
LarryH1229 <145599600+LarryH1229@users.noreply.github.com>
2023-09-20 13:52:54 -0400

    First commit

--- P1LAB1_HurleyLarry.py
+++ P1LAB1_HurleyLarry.py
@@ -1,10 +1,2 @@
-Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-====================================================================================== RESTART: C:/Users/hurleyl2278/Desktop/P1LAB1_HurleyLarry.py =====================================================================================
-
-========== RESTART: C:/Users/hurleyl2278/Desktop/P1LAB1_HurleyLarry.py =========
-Larry
-HELLO Larry and welcome to CS Online!
->>> 
-========== RESTART: C:/Users/hurleyl2278/Desktop/P1LAB1_HurleyLarry.py =========
+user_name = input()
+print("Hello", user_name,'and welcome to CS online')
