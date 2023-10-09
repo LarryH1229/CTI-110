f0e570978350edd3926f972a19f5bb46a78b0aa1
LarryH1229 <145599600+LarryH1229@users.noreply.github.com>
2023-10-09 14:06:19 -0400

    Add files via upload

---
+++ P4HW2_HurleyLarry.py
@@ -0,0 +1,48 @@
+#Larry Hurley
+#10-4-23, 10-9-23 V.2
+#Use if else statements to determine gross pay
+
+name = ""
+count = 0
+total_pay = 0
+
+
+while name != "Done":
+#Step 1 Get input from the user
+    name= input("What is the employee's name: ")
+    if name != "Done":
+        count = count + 1
+        num_hours = float(input("Enter number of hours worked:"))
+        pay_rate = float(input("Enter the hourly pay rate:"))
+        OT_hours = 0
+        has_OT = False
+        OT_pay = 0
+        reg_pay = 0
+        #Step 2Calculate overtime = yes/no - how much
+        if num_hours > 40:
+            has_OT = True
+            OT_hours = num_hours - 40
+        else:
+            has_OT = False
+
+        if has_OT == True:
+            OT_pay = (pay_rate*1.5) * (num_hours - 40)#Actual OT total pay
+        else:
+            OT_pay = 0
+            
+        #Calculate regular pay
+        reg_pay = pay_rate * num_hours - OT_hours
+        gross_pay = reg_pay + OT_pay
+        total_pay = total_pay + gross_pay
+
+        # Display name, pay rate, num hours worked,OT hours, OT pay,regular gross pay
+
+        print("Name:",name)
+        print("Overtime Hours:",OT_hours)
+        print("Overtime pay:",OT_pay)
+        print("Regular hours:",num_hours - OT_hours)
+        print("Regular pay:",reg_pay)
+        print("Gross pay:",reg_pay + OT_pay)
+#While loop breaks
+print("Total number of employees entered:", count)
+print("Total paid to the employee's:", total_pay)
